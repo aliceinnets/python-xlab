9b29354b3e31a36486dda5f044e818ba8513631c
aliceinnets <aliceinnets@gmail.com>
2017-04-02 22:45:56 +0100

    tested tf in different ways for mnist

--- xlab/play/tensorflow/deep_mnist.py
+++ xlab/play/tensorflow/deep_mnist.py
@@ -6,74 +6,46 @@ Created on 1 Apr 2017
 
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import numpy as np
+import util.oneliners as oneliners
 
 def main():
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-#     training(simple_model, mnist)
-    training_adam_dropout(conv_model, mnist)
+#     param = {}
+#     training(mnist, simple_model, param)
+#     np.savetxt(oneliners.test_results_path+'aW.txt', param['W'].eval())
+#     np.savetxt(oneliners.test_results_path+'ab.txt', param['b'].eval())
+    
+    param = {}
+    param['W'] = np.loadtxt(oneliners.test_results_path+'aW.txt', np.float32)
+    param['b'] = np.loadtxt(oneliners.test_results_path+'ab.txt', np.float32)
+    test(mnist, simple_model, param)
+#     training(mnist, simple_model, param)
+#     test(mnist, simple_model, param)
+    
+#     np.savetxt(oneliners.test_results_path+'aW.txt', param['W'].eval())
+#     np.savetxt(oneliners.test_results_path+'ab.txt', param['b'].eval())
+    
+#     training_adam_dropout(conv_model, mnist)
 #     run()
 
-def run():
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+def training(data, model, param):
+    input_dim = np.size(data.train.next_batch(1)[0], 1)
+    output_dim = np.size(data.train.next_batch(1)[1], 1)
     
     sess = tf.InteractiveSession()
     
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    y_ = tf.placeholder(tf.float32, shape=[None, 10])
+    x = tf.placeholder(tf.float32, shape=[None, input_dim])
+    y_ = tf.placeholder(tf.float32, shape=[None, output_dim])
     
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
+    if len(param) == 0:
+        param['W'] = tf.Variable(tf.zeros([784,10]))
+        param['b'] = tf.Variable(tf.zeros([10]))
+    else:
+        param['W'] = tf.Variable(param['W'])
+        param['b'] = tf.Variable(param['b'])
     
-    x_image = tf.reshape(x, [-1,28,28,1])
-    
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-    
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-    
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
-    
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-    
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
-    
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-    
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    
-    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    
-    sess.run(tf.global_variables_initializer())
-
-    for i in range(2000):
-        batch = mnist.train.next_batch(50)
-        if i%100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(i, train_accuracy))
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-    print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-    
-    
-def training(model, data):
-    sess = tf.InteractiveSession()
-    
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    y_ = tf.placeholder(tf.float32, shape=[None, 10])
-    
-    y = model(x)
+    y = model(x, param)
     
     sess.run(tf.global_variables_initializer())
     
@@ -84,6 +56,27 @@ def training(model, data):
         batch = data.train.next_batch(100)
         train_step.run(feed_dict={x: batch[0], y_: batch[1]})
         
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
+    print(accuracy.eval(feed_dict={x: data.test.images, y_: data.test.labels}))
+    
+        
+def test(data, model, param):
+    input_dim = np.size(data.train.next_batch(1)[0], 1)
+    output_dim = np.size(data.train.next_batch(1)[1], 1)
+    
+    sess = tf.InteractiveSession()
+    
+    
+    
+    x = tf.placeholder(tf.float32, shape=[None, input_dim])
+    y_ = tf.placeholder(tf.float32, shape=[None, output_dim])
+    
+    y = model(x, param)
+    
+    sess.run(tf.global_variables_initializer())
+    
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     
@@ -116,15 +109,10 @@ def training_adam_dropout(model, data):
 
     print("test accuracy %g"%accuracy.eval(feed_dict={x: data.test.images, y_: data.test.labels, keep_prob: 1.0}))
 
-def simple_model(x, keep_drop):
-    W = tf.Variable(tf.zeros([784,10]))
-    b = tf.Variable(tf.zeros([10]))
-    
-    y = tf.matmul(x,W) + b
-    
-    return y
+def simple_model(x, param):
+    return tf.matmul(x,param['W']) + param['b']
 
-def conv_model(x, keep_prob):
+def conv_model(x, *keep_prob):
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     
--- xlab/play/tensorflow/mnist_softmax.py
+++ xlab/play/tensorflow/mnist_softmax.py
@@ -27,6 +27,7 @@ import sys
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
+# import util.oneliners as oneliners
 
 FLAGS = None
 
@@ -68,9 +69,12 @@ def main(_):
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+    
+#     file_writer = tf.summary.FileWriter(oneliners.test_results_path+'/path/to/logs', sess.graph)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data', help='Directory for storing input data')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
